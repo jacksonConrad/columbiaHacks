a641e510a13d588b4186d1c6f41b94326b5dc245
Jackson Foley <jacksoncfoley@gmail.com>
2014-02-08 07:24:29 -0500

    Implement front-end and node initialization

--- api/api.js
+++ api/api.js
@@ -16,117 +16,56 @@ module.exports = function(app, mongoose) {
 	// Create graph of connections according to users/:id/followings
 	app.get('/api/user/:username/:depth', function(req, res) {
 
+		var root = {};
 
-
-
-
-
-
-		
-		depthSearch(depth, node);
-
-
-		// initial user ID request
-		request(userURL + req.params.username + clientID, 'json', function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-			  	var user = JSON.parse(body);
-
-			   // res.json(user); // Print the google web page
-			    console.log(user.id);
-
-			    // Checks if user ID already exists in database
-			    ArtistNode.findOne({'id': user.id}, 'id username', function (err, result) {
-			    	console.log(result);
-			    	//console.log(err);
-			    	if (result == null) {
-			    		ArtistNode.create({
-			    			id: user.id,
-			    			username: user.username
-			    		}, function (err, results) {
-			    			if (err) {
-			    				console.log('ERR: unable to create user');
-			    			}
-			    			else {
-			    				console.log('niggabitch');
-
-			    				// GET request for user's favorite tracks
-			    				request(baseURL + user.id + '/favorites.json?client_id=ee6c012d3805b479acf430ce6e188fa5',
-    								function (error, response, body) {
-
-
-
-    								}
-    							);
-
-			    				// API call for user/:id/followings
-			    				request(baseURL + user.id + '/followings.json?client_id=ee6c012d3805b479acf430ce6e188fa5',
-			    					function (error, response, body) {
-			    						
-			    						
-
-			    						request(baseURL + user.id + '/favorites.json?client_id=ee6c012d3805b479acf430ce6e188fa5',
-		    								function (error, response, body) {
-		    									var results = JSON.parse(body);
-		    									res.json(results);
-		    								}
-			    						);
-
-			    						_.each(results, function (user) {
-			    							
-			    						});
-			    						
-
-
-
-
-
-
-
-			    					}
-			    				);
-
-			    				console.log('USER CREATED BITCH');
-
-
-
-			    			}
-			    		});
-			    	}
-			    });
-			}
-		});
-
-
-		// if user is in DB, retrieve
-
-		
-
-		// otherwise, get user JSON according to his ID
-		// get list of users being followed
-		// iterate through users.
-		// if they have >10,000 followers:
-		// check if they are in db.
-		// if not, make them a node		
-		// create an edge or iterate the edge value between child and parent node
-		// if they have <10,000 followers 
-		// if we aren't at the last level, recursion
+		async.series([
+			function () {
+				var root = getSCUser(req.params.username, clientID);
+			},
+			function () {
+				var node = findByID(root.id);
+			},
+			function () {
+				if ( countOutgoingEdges(node.id) ) {
+					// don't process (for now)
+				}
+				else {
+					// process favorites
+					getSCFavorites(node.id, clientID function (children) {
+						data.nodes = children;
+						createEdges(node, favorites, function () {
+							data.edges = findOutgoingEdges(node.id);
+						}
+					});
+				}
+			},
+			function () {
+				// aggregate data to send to front end
+				data.nodes.push(node);
+				// data =
+				// 		{
+				// 			nodes: {
+				// 				{}, 
+				// 				{}
+				// 			},
+				// 		
+				// 			edge: {
+				// 				{}, 
+				// 				{}
+				// 			}
+				// 		}
+				res.json(data);
+			}]
+		);
 	});
 }
 
-		/*	// Find associated edges, and child nodes associated with those edges
-			// If leaf, go more deeper
-			if (countOutgoingEdges(result.id) == 0) {
-				// go deeper
-			}
-			else {
-				// return edges & child nodes
-				findOutgoingEdges (id, function (result) {
-
-				});*/
 function getSCFavorites (userID, clientID, callback) {
 	request(baseURL + userID + '/favorites.json?client_id=ee6c012d3805b479acf430ce6e188fa5', 'json', function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 		  	var favorites = JSON.parse(body);
+		  	// create edges
+
 		  	return favorites;
 		}
 	});
@@ -147,6 +86,7 @@ function findByID (id, callback) {
 	ArtistNode.findOne({'id': id}, 'id username', function (err, result) {
 		if (result == null) {
 			// create user for DB and return user
+			return createArtistNode(id);
 		}
 		else {
 			// return user
@@ -169,7 +109,7 @@ function createArtistNode(id, callback) {
 // Find all the edges coming out of a node
 function findOutgoingEdges (id, callback) {
 	Edge.find({'nodeA': id}, 'nodeA nodeB weight', function (err, result) {
-		callback(result);
+		return result;
 	});
 }
 
@@ -178,12 +118,45 @@ function findOutgoingEdges (id, callback) {
 // Count all the edges coming out of a node
 function countOutgoingEdges (id, callback) {
 	Edge.count({'nodeA': id}, function (err, count) {
+		console.log(count);
 		return count;
 	});
 }
 
+// find edge
+function findEdge (parent, child) {
+	Edge.findOne({'nodeA': parent.id, 'nodeB': child.id}, function (err, results) {
+		if (results == null) { return 0; }
+		else { return 1; }
+	});
+}
+
+// create a single edge
+function addEdge (parent, child, callback) {
+	// if we find an edge, don't create another
+	if ( findEdge(parent, child) )
+		return;
+	// otherwise, create new edge between the parent and child
+	else {
+		Edge.create({
+			nodeA: parent,
+			nodeB: child
+		}, function (err, edge) {
+			return edge;
+		});
+	}
+}
+
+// creates edges between a parent and its children
+function createEdges (parent, children, callback) {
+	_.each(children, function (child) {
+		addEdge(parent, child);
+	});
+	callback();
+}
+
 function depthSearch (user, callback) {
-	findByID	
+	//findByID	
 
 }
 
