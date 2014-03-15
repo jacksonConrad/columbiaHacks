c98131642a1f33f107c2e77028120a71bfce92ab
Jackson Foley <jacksoncfoley@gmail.com>
2014-03-15 16:42:36 -0400

    update services

--- public/js/services.js
+++ public/js/services.js
@@ -5,8 +5,35 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', ['ngResource']).
-	factory('tweetService', function($resource) {
+angular.module('myApp.services', []).
+	factory('scService', function() {
+		// initialize client with app credentials
+	
+
+	return {
+		init: function () {
+			SC.initialize({
+			  client_id: 'e2e194f256c97b3b526c2ba93db1d19d',
+			  // Only needed for authentication
+			  //redirect_uri: 'REDIRECT_URL'
+			});
+		},
+		createPlaylist: function(tracks, name, callback) {
+			SC.connect(function() {
+			  var t = nodes.map(function(node) { return { id: node.id } });
+			  SC.post('/playlists', {
+			    playlist: { title: name, tracks: t }
+			  });
+			  var playlistURL = 'www.soundcloud.com/discoversc/sets/' + name;
+			  callback(null,playlistURL);
+			});
+		}
+	}
+
+
+
+	});
+	/*factory('tweetService', function($resource) {
 		return $resource('/api/getlast/:number', {}, {
 			// Use this method for getting a list of tweets
 			
@@ -36,4 +63,4 @@ angular.module('myApp.services', ['ngResource']).
 	    }
 		};
 	// Service to load the d3 library onto the page without having it in index.html directly
-	})
+	});*/
